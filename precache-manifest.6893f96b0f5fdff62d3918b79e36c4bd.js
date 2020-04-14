71e0559ae29ba5eae021d0a6730eb7fee722ec21
Rama krishna <ramakrishna@Ramas-MacBook-Pro.local>
2020-04-14 23:59:03 +0530

    Updates

---
+++ precache-manifest.6893f96b0f5fdff62d3918b79e36c4bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a25601ee5817d449caf2a6ddbf7fa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b89594f85b9d470cb73",
+    "url": "/static/css/main.8564453d.chunk.css"
+  },
+  {
+    "revision": "a29cd5134825508e6f9d",
+    "url": "/static/js/2.629486d2.chunk.js"
+  },
+  {
+    "revision": "67da4c5460ef450f09fc1f206bc6b927",
+    "url": "/static/js/2.629486d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b89594f85b9d470cb73",
+    "url": "/static/js/main.d0f08b3c.chunk.js"
+  },
+  {
+    "revision": "f761eb67edc7c76c5d97",
+    "url": "/static/js/runtime-main.9a2149a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8dbeeb58d5a48cb706ae2fc6661c4118",
+    "url": "/static/media/virtual-dom.8dbeeb58.png"
+  }
+]);
